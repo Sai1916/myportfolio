fedbc5ee4cb176a95223f4db7cc8fb8a99566a22
Sai Sumedh <52703087+Sai1916@users.noreply.github.com>
2021-07-01 13:13:00 +0530

    Update EduDetails.js

--- src/components/EduDetails.js
+++ src/components/EduDetails.js
@@ -17,7 +17,7 @@ const EduDetails = ({ title, university, year, logo }) => {
                     {year}
                 </Year>
             </DetailsConatiner>
-            <Image width={320} objectFit="contain" height={60} src={logo} alt="logo" />
+            <Logo width={320} className={styles.logo} objectFit="contain" height={60} src={logo} alt="logo" />
         </Container>
     )
 }
@@ -46,11 +46,26 @@ const DetailsConatiner = styled.div`
 const Title = styled.h3`
     font-size: 28px;
     font-weight: 700px;
+    @media (max-width:425px){
+        font-size: 20px;
+    }
 `
 const CollegeName = styled.p`
     font-size:18px; 
     font-weight: bold;
+    @media (max-width:425px){
+       font-size: 16px;
+    }
 `
 const Year = styled.p`
     font-weight: 700;
+    @media (max-width:425px){
+        font-size: 16px;
+    }
+`
+const Logo = styled(Image)`
+     @media (max-width:425px){
+      width: 100%;
+      height: 80%;
+  }
 `
