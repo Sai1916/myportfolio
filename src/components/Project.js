fd0ce3eba7e22b894912cf429136645498e8a446
Sai Sumedh <52703087+Sai1916@users.noreply.github.com>
2021-07-12 12:48:37 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,7 +9,7 @@ function Project({title,description,image,repo,demo}) {
             <Title>
                 {title}
             </Title>
-            <Image className={styles.projectImage} objectFit="contain" width="270%" height="180%" src={image} alt="image"/>mage"/>
+            <Image className={styles.projectImage} objectFit="contain" width="270%" height="180%" src={image} alt="image"/>
             <Description>
                 {description}
             </Description>
