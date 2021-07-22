3fa9340606d1fcf4a6315778afff1c10c87acc54
Sai Sumedh <52703087+Sai1916@users.noreply.github.com>
2021-07-22 11:45:10 +0530

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -2,6 +2,7 @@ export const data = [
     {
         id: 1,
         title:"Bachelor's Degree",
+        branch: "CSE with Spec. in Networking and Security",
         university:'VIT-AP University, Andhra Pradesh, India',
         year:'2018-22',
         logo:'/vitapLogo.png'
